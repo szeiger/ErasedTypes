58e84da12c05a0ab39fa5f29134ffaf1a56cef6f
Stefan Zeiger <szeiger@novocode.com>
2012-02-06 12:10:40 +0100

    Trying to get rid of the extra type constructor in KCons (does not yet compile)

--- src/main/scala/com/novocode/erased/KList.scala
+++ src/main/scala/com/novocode/erased/KList.scala
@@ -3,16 +3,17 @@ package com.novocode.erased
 // KList
 
 sealed trait KList { kl =>
-  type Self <: KList
+  type LikeThis = KList { type Cons[X] = kl.Cons[X] }
+  type Self <: LikeThis
   type Cons[+_]
   type Head
-  type Tail <: KList
+  type Tail <: LikeThis
   type Fold[U, F[_ <: KList, _ <: U] <: U, Z <: U] <: U
   
   type Drop[N <: Nat] = N#Fold[KList, ({ type L[X <: KList] = X#Tail })#L, Self]
   type Apply[N <: Nat] = ({ type L[X <: KList] = X#Head })#L[Drop[N]] // Drop[N]#Head
   type Length = Fold[Nat, ({ type L[X <: KList, Z <: Nat] = Succ[Z] })#L, Nat._0]
-  type |: [E] = KCons[Cons, E, Self]
+  type |: [E] = KCons[E, Self]
   type |:: [L <: KList] = L#Fold[KList, ({ type L[X <: KList, Z <: KList] = Z # |: [X#Head] })#L, Self]
   type AsIdentity = Fold[KList, ({ type L[X <: KList, Z <: KList] = Z # |: [X#Cons[X#Head]] })#L, KNil[KList.Identity]]
   type Map[To[+_]] = Fold[KList, ({ type L[X <: KList, Z <: KList] = Z # |: [X#Head] })#L, KNil[To]]
@@ -22,15 +23,12 @@ sealed trait KList { kl =>
   def self: Self
   def fold[U, F[_ <: KList, _ <: U] <: U, Z <: U](f: TypedFunction2[KList, U, U, F], z: Z): Fold[U, F, Z]
 
-  final def map[To[+_]](f: NaturalTransformation1[Cons, To]): Self#Map[To] = self.fold[KList, ({ type L[X <: KList, Z <: KList] = Z # |: [X#Head] })#L, KNil[To]](
+  final def map[To[+_]](f: NaturalTransformation1[Cons, To]): Self#Map[To] = ??? /*self.fold[KList, ({ type L[X <: KList, Z <: KList] = Z # |: [X#Head] })#L, KNil[To]](
       new TypedFunction2[KList, KList, KList, ({ type L[X <: KList, Z <: KList] = Z # |: [X#Head] })#L] {
-        def apply[X <: KList, Z <: KList](x: X, z: Z): (Z # |: [X#Head]) = {
-          val tf = f.asInstanceOf[NaturalTransformation1[x.Cons, z.Cons]]
-          val newEU = tf.apply[X#Head](x.head)
-          val newE: z.Cons[X#Head] = newEU
-          newE |: z
+        def apply[X <: KList, Z <: KList { type Cons[X] = To[X] }](x: X, z: Z) = {
+          f.apply[x.Head](x.head) |: z
         }
-      }, KNil[To])
+      }, KNil[To])*/
 
   final def asIdentity = this.asInstanceOf[Self#AsIdentity]
   final def length: Length = {
@@ -38,7 +36,7 @@ sealed trait KList { kl =>
     foreach { _ => i += 1 }
     Nat.unsafe[Length](i)
   }
-  final def |: [@specialized E](elem: Cons[E]): |: [E] = new KCons[Cons, E, Self](elem, this.asInstanceOf[Self])
+  final def |: [@specialized E](elem: Self#Cons[E]): |: [E] = new KCons[E, Self](elem, self)
   /*final def |:: [L <: KList { type Cons[X] = kl.Cons[X] }](l: L): |:: [L] = l.fold[KList { type Cons[X] = kl.Cons[X] }, ({ type L[X <: KList { type Cons[X] = kl.Cons[X] }, Z <: KList { type Cons[X] = kl.Cons[X] }] = Z # |: [X#Head] })#L, Self](
       new TypedFunction2[KList { type Cons[X] = kl.Cons[X] }, KList { type Cons[X] = kl.Cons[X] }, KList { type Cons[X] = kl.Cons[X] }, ({ type L[X <: KList { type Cons[X] = kl.Cons[X] }, Z <: KList { type Cons[X] = kl.Cons[X] }] = Z # |: [X#Head] })#L] {
         def apply[P1 <: KList, P2 <: KList { type Cons[X] = kl.Cons[X] }](p1: P1, p2: P2) = p1.head |: p2
@@ -49,15 +47,15 @@ sealed trait KList { kl =>
     var i = n.value
     while(i > 0) {
       i -= 1
-      t = t.asInstanceOf[KCons[KList.AnyCons, _,_ <: KList]].tail 
+      t = t.asInstanceOf[KCons[_,_ <: KList]].tail 
     }
     t
   }.asInstanceOf[Drop[N]]
   final def apply [N <: Nat](n: N) = drop(n).head.asInstanceOf[Cons[Apply[N]]]
   final def foreach(f: Any => Unit) {
     var n: KList = this
-    while(n.isInstanceOf[KCons[Cons,_,_]]) {
-      val c = n.asInstanceOf[KCons[Cons, _, _ <: KList]]
+    while(n.isInstanceOf[KCons[_,_]]) {
+      val c = n.asInstanceOf[KCons[_, _ <: KList]]
       f(c.head)
       n = c.tail
     }
@@ -74,15 +72,15 @@ sealed trait KList { kl =>
 
 final object KList {
   type AnyCons[+X] = A[X] forSome { type A[+_] }
-  type |: [F[+_], H, T <: KList { type Cons[X] = F[X] }] = KCons[F, H, T]
-  type ||: [F[+_], H, N] = KCons[F, H, KCons[F, N, KNil[F]]]
+  type |: [H, T <: KList] = KCons[H, T]
+  type ||: [F[+_], H, N] = KCons[H, KCons[N, KNil[F]]]
   type Identity[+X] = X
   type KHList = KList { type Cons[X] = X }
 }
 
-final class KCons[F[+_], @specialized H, T <: KList](val head: F[H], val tail: T) extends KList {
-  type Cons[+X] = F[X]
-  type Self = KCons[Cons, H, T]
+final class KCons[H, T <: KList](val head: T#Cons[H], val tail: T) extends KList { kl =>
+  type Cons[+X] = T#Cons[X]
+  type Self = KCons[H, T]
   type Head = H
   type Tail = T
   type Fold[U, F[_ <: KList, _ <: U] <: U, Z <: U] = F[Self, T#Fold[U, F, Z]]
--- src/main/scala/com/novocode/erased/TypedFunction.scala
+++ src/main/scala/com/novocode/erased/TypedFunction.scala
@@ -5,6 +5,6 @@ package com.novocode.erased
  */
 trait TypedFunction
 
-trait TypedFunction2[T1, T2, TR, F[_ <: T1, _ <: T2] <: TR] extends TypedFunction {
+trait TypedFunction2[-T1, -T2, +TR, F[_ <: T1, _ <: T2] <: TR] extends TypedFunction {
   def apply[P1 <: T1, P2 <: T2](p1: P1, p2: P2): F[P1, P2]
 }
