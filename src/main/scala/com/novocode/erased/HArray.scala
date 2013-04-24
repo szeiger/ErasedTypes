2257dd3ddf3d83fd3b426f58727b4ae415499fdd
Stefan Zeiger <szeiger@novocode.com>
2013-04-24 18:02:58 +0200

    Add _1, _2, … accessor methods to HArray.
    This is done in a safe and efficient way by having HArray extend
    scala.Dynamic and implementing selectDynamic() with a macro.

--- src/main/scala/com/novocode/erased/HArray.scala
+++ src/main/scala/com/novocode/erased/HArray.scala
@@ -1,15 +1,20 @@
 package com.novocode.erased
 
+import scala.language.dynamics
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
+
 // HArray
 
-sealed trait HArray[L <: HList] extends Product {
-  def apply[N <: Nat](n: N): L#Apply[N]
+sealed trait HArray[L <: HList] extends Any with Product with Dynamic {
+  @inline final def apply[N <: Nat](n: N) = unsafeApply[N](n.value)
+  def unsafeApply[N <: Nat](n: Int): L#Apply[N]
   def length: L#Length
   def productArity = 5
   def canEqual(that: Any) = that.isInstanceOf[HArray[_]]
   override def equals(that: Any): Boolean = that match {
     case h: HArray[_] =>
-      var l = productArity
+      val l = productArity
       if(l != h.productArity) return false
       var i = 0
       while(i < l) {
@@ -19,6 +24,7 @@ sealed trait HArray[L <: HList] extends Product {
       true
     case _ => false
   }
+  def selectDynamic(field: String): Any = macro HArray.selectDynamicImpl
 }
 
 object HArray {
@@ -27,10 +33,32 @@ object HArray {
   def apply[T1](v1: T1) = unsafe[T1 |: HNil](Array(v1))
   def apply[T1, T2](v1: T1, v2: T2) = unsafe[T1 ||: T2](Array(v1, v2))
   def apply[T1, T2, T3](v1: T1, v2: T2, v3: T3) = unsafe[T1 |: T2 ||: T3](Array(v1, v2, v3))
+
+  def selectDynamicImpl(ctx: Context)(field: ctx.Expr[String]): ctx.Expr[Any] = {
+    import ctx.universe._
+    val idx = {
+      val Expr(Literal(Constant(n: String))) = field
+      (if(n.startsWith("_")) {
+          val i = try Some(n.substring(1).toInt) catch { case e: NumberFormatException => None }
+          i.flatMap(v => if(v < 1) None else Some(v-1))
+        } else None
+      ).getOrElse(ctx.abort(field.tree.pos, "value "+n+" is not a member of "+classOf[HArray[_]].getName))
+    }
+
+    val _Succ = typeOf[Succ[_]].typeSymbol
+    val _Zero = typeOf[Zero.type].typeSymbol.companionSymbol
+    val natT = (1 to idx).foldLeft(SingletonTypeTree(Ident(_Zero)): ctx.Tree) { case (z, _) =>
+      AppliedTypeTree(Ident(_Succ), List(z))
+    }
+
+    val t = Apply(TypeApply(Select(ctx.prefix.tree, newTermName("unsafeApply")),
+      List(natT)), List(Literal(Constant(idx))))
+    ctx.Expr[Any](t)
+  }
 }
 
-final class HArrayA[L <: HList](a: Array[Any]) extends HArray[L] {
-  def apply[N <: Nat](n: N) = a(n.value).asInstanceOf[L#Apply[N]]
+final class HArrayA[L <: HList](val a: Array[Any]) extends AnyVal with HArray[L] {
+  def unsafeApply[N <: Nat](n: Int) = a(n).asInstanceOf[L#Apply[N]]
   def length = a.length.asInstanceOf[L#Length]
   override def toString = a.mkString("(",",",")")
   def productElement(n: Int) = a(n)
--- src/test/scala/com/novocode/erased/test/HArrayTest.scala
+++ src/test/scala/com/novocode/erased/test/HArrayTest.scala
@@ -8,8 +8,15 @@ class HArrayTest {
   def testHArray {
     val v1 = HArray("foo", 42, true)
     println(v1)
+
+    // Access by Nat index (0-based)
     println(v1(Nat._0): String)
     println(v1(Nat._1): Int)
     println(v1(Nat._2): Boolean)
+
+    // Access by macro-generated accessors (1-based)
+    println(v1._1: String)
+    println(v1._2: Int)
+    println(v1._3: Boolean)
   }
 }
